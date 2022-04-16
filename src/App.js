261d9b9d0087fcc42440e4d48a9474fad7d48ebe
Zane Stanutz <stanutz,stanutz@gmail.com>
2022-04-16 19:14:16 -0400

    added Form component to Feed

--- src/App.js
+++ src/App.js
@@ -14,47 +14,23 @@ return (
     </>
   )
 }
-function Form(){
-  const [start, setStart] = useState("");
-  const[end, setEnd] = useState("");
-  const handleStartChange = e => {
-    setStart(e.target.value)
-  };
-  const handleEndChange = e => {
-    setEnd(e.target.value)
-  }
 
-  function handleSubmit(){
-    alert(`this is start : ${start}/n this is end : ${end}`); 
-  }
-  return(
-    <form onSubmit={handleSubmit}>
-          <label>Start Year</label>
-          <input 
-            id='start' 
-            type="date" 
-            name="start" 
-            onChange={handleStartChange} 
-            value={start} 
-            placeholder="yy-mm-dd" 
-            required/>
-          <label>End Year</label>
-          <input 
-            id='end' 
-            type="date" 
-            name="end"
-            onChange={handleEndChange}
-            value={end}
-            placeholder="yy-mm-dd" 
-            required/>
-          <button type='submit'>Find pictures</button>
-      </form>
-  )
-}
 function Feed(){
+  const [start, setStart] = useState("2022-01-01");
+  const[end, setEnd] = useState("2022-03-01");
   const[data, setData] = useState(null);
+  
+  function handleSubmit(){
+    const handleStartChange = e => {
+      setStart(e.target.value)
+    };
+    const handleEndChange = e => {
+      setEnd(e.target.value)
+    }
+  }
+  
   useEffect(() => {
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=e8K3ePSnMPazfDhsoTb4hJwJiu3yO3gCIY2zd6cb`)
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=e8K3ePSnMPazfDhsoTb4hJwJiu3yO3gCIY2zd6cb&start_date=${start}&end_date=${end}`)
       .then(res => res.json())
       .then(setData)
       .catch(console.error);
@@ -62,12 +38,57 @@ function Feed(){
   if(data){
     return(
       <>
+        <form onSubmit={handleSubmit}>
+              <label>Start Year</label>
+              <input 
+                id='start' 
+                type="date" 
+                name="start" 
+                value={start} 
+                placeholder="yy-mm-dd" 
+                required/>
+              <label>End Year</label>
+              <input 
+                id='end' 
+                type="date" 
+                name="end"
+                value={end}
+                placeholder="yy-mm-dd" 
+                required/>
+              <button type='submit'>Find pictures</button>
+          </form>
         <div className="Feed">
           <img className="FeedImg"src={data.hdurl}></img>
           <h3>{data.title}</h3><p>{data.date}</p>
           <p>{data.explanation}</p>
         </div>
       </>
+    )
+  }else{
+    return(
+      <>
+        <form onSubmit={handleSubmit}>
+            <label>Start Year</label>
+            <input 
+              id='start' 
+              type="date" 
+              name="start" 
+              value={start} 
+              placeholder="yy-mm-dd" 
+              required/>
+            <label>End Year</label>
+            <input 
+              id='end' 
+              type="date" 
+              name="end"
+              value={end}
+              placeholder="yy-mm-dd" 
+              required/>
+            <button type='submit'>Find pictures</button>
+        </form>
+        <h2>Nasa is down :( </h2>
+      </>
+
     )
   }
 }
@@ -77,9 +98,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <Form/>
       <Feed/>
-
     </div>
   );
 }
